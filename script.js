a2d457b92d3ca66ef4e3e5a8823611e5ca7f3a58
FELIPE ALVES RODRIGUES <00001106743088sp@aluno.educacao.sp.gov.br>
2025-08-28 15:49:43 -0300

    Implementando o curso de IA

--- script.js
+++ script.js
@@ -52,7 +52,8 @@ input.addEventListener("keydown", async function (event) {
             await typeText("[1] Curso: Análise e Desenvolvimento de Sistemas | Duração: 3 anos");
             await typeText("[2] Curso: Ciências da Computação | Duração: 4 anos");
             await typeText("[3] Curso: Cybersegurança | Duração: 3 anos");
-            await typeText("\n>>> Deseja acessar mais detalhes de um curso? Digite o número [1-3]");
+            await typeText("[4] Curso: Inteligência Artificial | Duração: 4 anos");
+            await typeText("\n>>> Deseja acessar mais detalhes de um curso? Digite o número [1-4]");
         }
 
         //Usuário escolhe o curso
@@ -74,7 +75,13 @@ input.addEventListener("keydown", async function (event) {
             await typeText("- Tecnologias: Pentest, criptografia, firewalls, SIEM");
             await typeText("- Mercado: Analista SOC, consultor de segurança, ethical hacker");
         }
+        else if (valor === "4"){
+            await typeText("\n>>> Detalhes de Inteligência Artificial:");
+            await typeText("\n - Foco: Desenvolvimento de sistemas inteligentes");
+            await typeText("\n - Tecnologias: Machine Learning, Deep Learning, NLP, Python");
+            await typeText("\n - Mercado: Cientista de dados, engenheiro de ML, pesquisador em IA");
 
+        }
         //Senha errada
         else {
             await typeText("\n>>> Intrusão detectada. Tente novamente.");
