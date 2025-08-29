91281d4946f3a36e36de09cb6ef7a0adf8c7c8de
FELIPE ALVES RODRIGUES <00001106743088sp@aluno.educacao.sp.gov.br>
2025-08-29 14:29:44 -0300

    Adicionando mais coisisnhas

--- script.js
+++ script.js
@@ -41,16 +41,39 @@ input.addEventListener("keydown", async function (event) {
             await typeText("\n>>> Acesso concedido.");
             await typeText(">>> Invadindo o banco de dados...");
             await new Promise(r => setTimeout(r, 1000));
-            await typeText(">>> SELECT * FROM cursos_TI WHERE ativo = 1;");
+
+            // ASCII art da UNIMAR
+            await typeText(`
+ $$\\   $$\\           $$\\                                   
+$$ |  $$ |          \\__|                                  
+$$ |  $$ |$$$$$$$\\  $$\\ $$$$$$\\$$$$\\   $$$$$$\\   $$$$$$\\  
+$$ |  $$ |$$  __$$\\ $$ |$$  _$$  _$$\\  \\____$$\\ $$  __$$\\ 
+$$ |  $$ |$$ |  $$ |$$ |$$ / $$ / $$ | $$$$$$$ |$$ |  \\__|
+$$ |  $$ |$$ |  $$ |$$ |$$ | $$ | $$ |$$  __$$ |$$ |      
+\\$$$$$$  |$$ |  $$ |$$ |$$ | $$ | $$ |\\$$$$$$$ |$$ |      
+ \\______/ \\__|  \\__|\\__|\\__| \\__| \\__| \\_______|\\__|      
+                                                          
+                                                          
+                                                          
+    `, 2); // delay rápido para o desenho sair fluído
+
+            await typeText("\n>>> Acesso privilegiado concedido.");
+            await typeText(">>> Dados confidenciais carregados do sistema UNIMAR.");
+            await typeText("------------------------------------------------------");
+            await typeText("- Fundada em: 1988");
+            await typeText("- Local: Marília/SP");
+            await typeText("- Cursos de destaque: TI, Direito, Medicina, Engenharias");
+            await typeText("- Parceiros: Cisco, AWS Academy, IBM SkillsBuild");
+            await typeText("------------------------------------------------------");
+
+            await typeText("\n>>> SELECT * FROM cursos_TI WHERE ativo = 1;");
             await new Promise(r => setTimeout(r, 1000));
             await typeText(">>> Conexão estabelecida.");
-            await new Promise(r => setTimeout(r, 1000));
             await typeText(">>> 3 registros encontrados.\n");
-            await typeText("[1] Curso: Análise e Desenvolvimento de Sistemas | Duração: 3 anos");
+            await typeText("[1] Curso: ADS | Duração: 3 anos");
             await typeText("[2] Curso: Ciências da Computação | Duração: 4 anos");
             await typeText("[3] Curso: Cybersegurança | Duração: 3 anos");
-            await typeText("[4] Curso: Inteligência Artificial | Duração: 4 anos");
-            await typeText("\n>>> Deseja acessar mais detalhes de um curso? Digite o número [1-4]");
+            await typeText("\n>>> Deseja acessar mais detalhes de um curso? Digite o número [1-3]");
         } else if (valor === "1") {
             await typeText("\n>>> Detalhes do curso ADS:");
             await typeText("- Foco: Desenvolvimento de sistemas e softwares");
